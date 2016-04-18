eeb12cd35e63ff3e0c763818936b68b7b2a8546c
Robert-IMT <robert.bogach@gmail.com>
2016-04-18 11:00:13 +0300

    Create module structure

---
+++ brig_real_shop/app/code/local/BRIg/sbBestsellers/Block/Monblock.php
@@ -0,0 +1,8 @@
+<?php
+class BRIg_sbBestsellers_Block_Monblock extends Mage_Core_Block_Template
+{
+     public function methodblock()
+     {
+         return '' ;
+     }
+}
---
+++ brig_real_shop/app/code/local/BRIg/sbBestsellers/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+class BRIg_sbBestsellers_IndexController extends Mage_Core_Controller_Front_Action
+{
+   public function indexAction ()
+   {
+		$this->loadLayout();
+		$this->renderLayout();
+   }
+   public function mamethodeAction ()
+   {
+     // 
+    }
+}
